00c0596b10f596f42041e3ca2640ca7e1218367c
abdullayvsaidjon-ux <abdullayvsaidjon@gmail.com>
2026-01-28 16:37:49 +0500

    k

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("20" + 120 - 40 + "30")
+
+
+
+window.onload=function(){
+    this.alert("120" - "20")
+}
+
+
+
+
+window.onload=function(){
+    this.alert("10" - "20" + "Teacher")
+}
+
+
+
+
+const a="30"
+const b="100"
+let result=a-b
+console.log(result)
+
+
+
+
+
+
+const f="500"
+const g="300"
+
+
+let result1=f-g
+console.log(f-g)
+console.log(f-a)
+console.log(b-g)
+
