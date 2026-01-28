f6f6351821d4608b15d66875ce1b2885e315761c
abdullayvsaidjon-ux <abdullayvsaidjon@gmail.com>
2026-01-28 17:29:28 +0500

    oj

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ console.log("20" + 120 - 40 + "30")
 
 
 
-window.onload=function(){
-    this.alert("120" - "20")
-}
+// window.onload=function(){
+//     this.alert("120" - "20")
+// }
 
 
 
 
-window.onload=function(){
-    this.alert("10" - "20" + "Teacher")
-}
+// window.onload=function(){
+//     this.alert("10" - "20" + "Teacher")
+// }
 
 
 
@@ -25,7 +25,8 @@ console.log(result)
 
 
 
-alert("Hello no no " + alert("40" + "22"))
+// alert("Hello no no " + alert("40" + "22")   + alert("go" + "jo" + "satu" + "run"))
+
 
 
 
@@ -39,4 +40,45 @@ let result1=f-g
 console.log(f-g)
 console.log(f-a)
 console.log(b-g)
+
+
+// alert("120" - "32" * "510" + "killer")
+
+
+let d="4"
+let dd="5"
+
+// alert(d/dd)
+
+
+
+
+// let.secundary=function()
+
+
+// let Totalsecunds="1200"
+// let Totalsecunds1="100"
+// let Totalsecunds2="120"
+// let Totalsecunds3="200"
+// let Totalsecunds4="10"
+// let Totalsecunds5="14"
+
+
+
+// alert("200" / Totalsecunds - "12")
+
+
+
+
+let secund="60"
+let min="60"
+let hour="24"
+let day="365"
+let year="100"
+let total=secund*min*hour*day*year
+
+alert("total "+ total)
+
+alert("new " + secund*min*hour*day*year*50)
+
 
