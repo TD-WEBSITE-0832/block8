2c6d41cde3a9004892588b283b43ef433994333c
abdullayvsaidjon-ux <abdullayvsaidjon@gmail.com>
2026-01-30 17:07:02 +0500

    dfsfdds

---
+++ script.js
@@ -0,0 +1,29 @@
+// document.getElementById("bt").onclick = function () {
+//   window.location.href = "neew.html";
+// };
+
+
+
+
+
+// let age =print('hello guys no ')
+// alert('yes hello guys' + age + 'killer old')
+// learnt
+
+
+
+
+
+// let ag =alert('hello guys no ')
+// prompt('yes hello guys' + ag + 'killer old')
+// learnt
+
+
+
+
+
+// let a =alert('hello guys no ')
+// prompt('yes hello guys' + a + 'killer old')
+// learnt
+
+
