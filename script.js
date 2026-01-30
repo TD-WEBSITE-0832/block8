6f2ca7c3f2bf71f5cfb6a17bf7d88d6f00cd286c
abdullayvsaidjon-ux <abdullayvsaidjon@gmail.com>
2026-01-30 17:22:29 +0500

    mat

--- script.js
+++ script.js
@@ -26,4 +26,25 @@
 // prompt('yes hello guys' + a + 'killer old')
 // learnt
 
+// let a=10
+// let b=32323
+// console.log(a+b)
 
+
+
+// let yes=alert('I have phone name samsung s55 5g')
+// confirm('now I have phone buy and me new phone name iphone 18 pro max')
+
+
+
+// let no=confirm('my name ')
+// alert(no)
+
+
+
+
+
+
+
+let mat=confirm('5*25')
+alert('125' +mat)
