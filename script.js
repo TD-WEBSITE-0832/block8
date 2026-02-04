7582e2c011a60e4bf9cdbc666561b61408924f89
abdullayvsaidjon-ux <abdullayvsaidjon@gmail.com>
2026-02-04 17:45:23 +0500

    salomlar bollar

--- script.js
+++ script.js
@@ -64,7 +64,105 @@
 // let mid=5*0
 // alert(mid)
 
+// let mid=12/2
+// confirm(mid+(22*2))
 
 
 
 
+// let x=20+3*(200+1)-(120-3000)
+// alert(x)
+
+// let a=20+3*(200+1)-(120-3000)
+// alert(x+a)
+
+
+
+
+
+
+// let d=0-6
+// alert(d)
+
+
+
+// let s=60<2
+// alert(s)
+
+
+
+
+// let confirm=prompt(70>3)
+// alert=confirm(prompt)
+
+
+
+let title=document.getElementById("txt")
+title.innerHTML="hello guys"
+title.style.fontSize="40px"
+title.style.color="red"
+title.style.textAlign="center"
+console.log(title)
+
+// let title=document.getElementById("txt")
+// title.style.color="red"
+// console.log(title)
+
+
+
+
+let photo=document.getElementById("ph")
+photo.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4kljr70EXGkgVxIBSgWFsQmy2QmJn_C1OqA&s"
+console.log(photo)
+photo.style.borderRadius="20px"
+photo.style.marginLeft="650px"
+photo.style.opacity="0.8"
+photo.addEventListener("mouseover", function() {
+  this.style.opacity = "0.5";
+  this.style.transition="all 0.6s ease-in-out"
+});
+photo.addEventListener("mouseout", function() {
+  this.style.opacity = "1";
+  this.style.transition="all 0.6s ease-in-out"
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let title1 = document.getElementById("txt1");
+
+title1.style.fontSize = "40px";
+title1.style.color = "#ff6400";
+title1.style.textAlign = "center";
+title1.innerHTML = "Plus buy ";
+
+let button = document.createElement("button");
+button.innerText = "Submit";
+button.id = "bt";
+
+button.style.padding = "10px";
+button.style.fontSize = "22px";
+button.style.marginLeft = "10px";
+button.style.cursor = "pointer";
+button.style.borderRadius="13px"
+button.style.border="none"
+button.style.width="140px"
+button.style.height="55px"
+
+title1.appendChild(button);
+
+console.log(button);
